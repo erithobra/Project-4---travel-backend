f0e89005d54f8159761a63055b9c88977c3d4f62
ericb2020 <ebrauer83@gmail.com>
2021-03-08 14:08:39 -0600

    creating server routes

--- server.js
+++ server.js
@@ -5,6 +5,33 @@ app.get("/", (req,res) => {
     res.send("here is your information");
 })
 
+const trips = [
+    {
+        startDate: "1/1/2016",
+        endDate: "1/14/2016",
+        destination: "US"
+    },
+    {
+        startDate: "1/1/2017",
+        endDate: "1/14/2017",
+        destination: "UK"
+    },
+    {
+        startDate: "1/1/2018",
+        endDate: "1/14/2018",
+        destination: "Germany"
+    }
+];
+
+// index route
+app.get("/trips/", (req, res) => {
+    res.send(trips);
+})
+
+//show route
+app.get("/trips/:id", (req, res) => {
+    res.send(trips[req.params.id])
+})
 
 app.listen(3000, () => {
     console.log("I am listening");
