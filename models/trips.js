a7c2d52ec0313e6734c1ba3f8b4c4529a52447ca
ericb2020 <ebrauer83@gmail.com>
2021-03-08 14:44:39 -0600

    install ejs

---
+++ models/trips.js
@@ -0,0 +1,19 @@
+const trips = [
+    {
+        startDate: "1/1/2016",
+        endDate: "1/14/2016",
+        destination: "US"
+    },
+    {
+        startDate: "1/1/2017",
+        endDate: "1/14/2017",
+        destination: "UK"
+    },
+    {
+        startDate: "1/1/2018",
+        endDate: "1/14/2018",
+        destination: "Germany"
+    }
+];
+
+module.exports = trips;
--- server.js
+++ server.js
@@ -1,27 +1,12 @@
 const express = require("express")
 const app = express()
+const trips = require("./models/trips.js")
 
 app.get("/", (req,res) => {
     res.send("here is your information");
 })
 
-const trips = [
-    {
-        startDate: "1/1/2016",
-        endDate: "1/14/2016",
-        destination: "US"
-    },
-    {
-        startDate: "1/1/2017",
-        endDate: "1/14/2017",
-        destination: "UK"
-    },
-    {
-        startDate: "1/1/2018",
-        endDate: "1/14/2018",
-        destination: "Germany"
-    }
-];
+
 
 // index route
 app.get("/trips/", (req, res) => {
@@ -30,8 +15,10 @@ app.get("/trips/", (req, res) => {
 
 //show route
 app.get("/trips/:id", (req, res) => {
-    res.send(trips[req.params.id])
-})
+    res.render("show.ejs", {
+        trip: trips[req.params.id],
+    });
+});
 
 app.listen(3000, () => {
     console.log("I am listening");
