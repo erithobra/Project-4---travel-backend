11e47c1593763e21a00a0d55811983be66c45673
ericb2020 <ebrauer83@gmail.com>
2021-03-09 10:24:21 -0600

    completed user sign up and log in

--- controllers/users.js
+++ controllers/users.js
@@ -6,13 +6,59 @@ const index = (req, res) => {
     });
 };
 
+const renderLogin = (req, res) => {
+    res.render("users/login.ejs")
+}
+
+const login = (req, res) => {
+    let index = users.findIndex(
+        user => (user.firstName === req.body.firstName &&
+            user.lastName === req.body.lastName && 
+            user.password === req.body.password)
+    )
+    res.redirect(`/users/${index}`);
+};
+
 const show = (req, res) => {
     res.render("users/show.ejs", {
         user: users[req.params.id],
     });
 };
 
+const renderNew = (req, res) => {
+    res.render("users/new.ejs");
+};
+
+const postNew = (req, res) => {
+    users.push(req.body);
+    res.redirect("/users");
+};
+
+const renderEdit = (req, res) => {
+    res.render("users/edit.ejs", {
+        user: users[req.params.id],
+        id: req.params.id
+    });
+};
+
+const putEdit = (req, res) => {
+    users[req.params.id] = req.body;
+    res.redirect("/users");
+};
+
+const deleteUser = (req, res) => {
+    users.splice(req.params.id, 1)
+    res.redirect("/users")
+};
+
 module.exports = {
     index,
     show,
+    renderNew,
+    postNew,
+    renderEdit,
+    putEdit,
+    deleteUser,
+    renderLogin,
+    login
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const ctrl = require("../controllers");
 
 router.get("/", ctrl.users.index);
+router.get("/login", ctrl.users.renderLogin);
+router.post("/login", ctrl.users.login);
+router.get("/new", ctrl.users.renderNew);
+router.post("/", ctrl.users.postNew);
+router.get("/:id/edit", ctrl.users.renderEdit);
+router.put("/:id", ctrl.users.putEdit);
+router.delete("/:id", ctrl.users.deleteUser);
 router.get("/:id", ctrl.users.show);
 
 module.exports = router;
